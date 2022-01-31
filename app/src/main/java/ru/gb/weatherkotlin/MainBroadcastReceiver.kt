f021f7bc3f4bdd4bdcfd318c60cee40aaa11e472
vikzak <victor@azbukait.ru>
2022-01-31 22:08:16 +0300

    ДЗ№6 + CONNECTIVITY_ACTION

---
+++ app/src/main/java/ru/gb/weatherkotlin/MainBroadcastReceiver.kt
@@ -0,0 +1,18 @@
+package ru.gb.weatherkotlin
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MainBroadcastReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        StringBuilder().apply {
+            append("СООБЩЕНИЕ ОТ СИСТЕМЫ\n")
+            append("Action: ${intent.action}")
+            toString().also {
+                Toast.makeText(context, it, Toast.LENGTH_LONG).show() }
+        }
+
+    }
+}
--- app/src/main/java/ru/gb/weatherkotlin/view/MainActivity.kt
+++ app/src/main/java/ru/gb/weatherkotlin/view/MainActivity.kt
@@ -1,7 +1,11 @@
 package ru.gb.weatherkotlin.view
 
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.gb.weatherkotlin.MainBroadcastReceiver
 import ru.gb.weatherkotlin.R
 import ru.gb.weatherkotlin.databinding.MainActivityBinding
 //import ru.gb.weatherkotlin.view.details.DetailsFragment
@@ -10,17 +14,28 @@ import ru.gb.weatherkotlin.view.main.MainFragment
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: MainActivityBinding
+    private val receiver = MainBroadcastReceiver()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
         //binding.ok.setOnClickListener(clickListener)
+        //registerReceiver(receiver, IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED))
+        val filter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION).apply {
+            addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+        }
+        registerReceiver(receiver, filter)
+
         if (savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commit()
         }
+    }
+    override fun onDestroy() {
+        unregisterReceiver(receiver)
+        super.onDestroy()
     }
 }
